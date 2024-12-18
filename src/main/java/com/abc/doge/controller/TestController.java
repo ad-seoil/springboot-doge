8ad96ec9293df48a0088e2aeb45eeaff3c16d8ee
ad.seoil <ad.seoil0112@gmail.com>
2024-12-18 12:11:50 +0900

    TestController추가

---
+++ src/main/java/com/abc/doge/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.abc.doge.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String test(){
+        return "Hello World!";
+    }
+
+}
