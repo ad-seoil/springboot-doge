a4608ebbe3e105a5611fec0924f4d0fad19b8356
ad.seoil <ad.seoil0112@gmail.com>
2024-12-18 11:51:51 +0900

    first commit

---
+++ src/test/java/com/abc/doge/DogeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.doge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
