ffa798a290def003b34c1c47c07a5c4252d85ad4
JoeClouser <qww5261715@gmail.com>
2024-12-27 15:17:29 +0900

    MemberInfo rebuild

--- src/main/java/com/abc/doge/entity/MemberInfo.java
+++ src/main/java/com/abc/doge/entity/MemberInfo.java
@@ -9,9 +9,29 @@ import lombok.Data;
 public class MemberInfo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer uid;    // UID
 
-    private String username;
-    private String password;
-    private String email;
+    @Column(name = "PW", nullable = false)
+    private String pw;  // password
+
+    @Column(name = "user_name", nullable = false)
+    private String userName;  // 사용자 이름
+
+    @Column(name = "birth_date", nullable = false)
+    private java.sql.Date birthDate;    // 생년월일
+
+    @Column(name = "email")
+    private String email;  // 이메일
+
+    @Column(name = "join_date", nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private java.sql.Timestamp joinDate; // 가입 날짜
+
+    @Column(name = "currency", nullable = false, columnDefinition = "int default 0")
+    private Integer currency; // 보유 재화
+
+    @Column(name = "user_level", nullable = false, columnDefinition = "int default 1")
+    private Integer userLevel; // 사용자 레벨
+
+    @Column(name = "user_exp", nullable = false, columnDefinition = "int default 0")
+    private Integer userExp; // 사용자 경험치
 }
