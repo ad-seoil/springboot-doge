029491aaed639e802444c817dff1155874c2e6d4
JoeClouser <qww5261715@gmail.com>
2024-12-27 15:19:57 +0900

    MemberResistrationDTO.java build

--- src/main/java/com/abc/doge/dto/MemberResistrationDTO.java
+++ src/main/java/com/abc/doge/dto/MemberResistrationDTO.java
@@ -4,7 +4,10 @@ import lombok.Data;
 
 @Data
 public class MemberResistrationDTO {
-  private String username;
-  private String password;
-  private String email;
+  private String id;                // 사용자 ID
+  private String pw;                // 비밀번호
+  private String userName;          // 사용자 이름
+  private java.sql.Date birthDate;  // 생년월일
+  private String nickname;          // 닉네임
+  private String email;             // 이메일
 }
