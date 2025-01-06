b47a0e3477375ed3b6be1e21cc4e3b1e3a244b14
ad.seoil <ad.seoil0112@gmail.com>
2025-01-06 09:12:41 +0900

    문의 메일 전송기능 구현 ㄴ병합하는 과정에서 에러 발생 발송 성공 페이지 구현 세로형 헤더 구현 프로필 페이지 작업중 2025.01.06 HSJ

--- src/main/java/com/abc/doge/controller/InquiryController.java
+++ src/main/java/com/abc/doge/controller/InquiryController.java
@@ -3,17 +3,18 @@ package com.abc.doge.controller;
 import com.abc.doge.dto.InquiryDto;
 import com.abc.doge.service.InquiryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
 public class InquiryController {
 
-    private final InquiryService inquiryService;
+    @Autowired
+    private InquiryService inquiryService;
 
     // 문의페이지 연결
     @GetMapping("/inquiry")
@@ -29,11 +30,13 @@ public class InquiryController {
             return "inquiry"; // 오류 발생 시 다시 폼으로 돌아감
         }
 
-        // 문의 메일을 입력받아 DB에 저장
-        inquiryService.send(inquiryDto);
+        // 문의 메일을 입력받아 DB에 저장 및 전송
+        inquiryService.sendEmail(inquiryDto);
+
+        model.addAttribute("message", "문의가 성공적으로 전송되었습니다.");
 
         // 결과 페이지로 리다이렉트
-        return "redirect:/inquiry";
+        return "inquiry_success";
     }
 
 }
