f6766a1476d75d15ea19022cc46bc593b7c3eb93
sora0301 <qoavkdldj128@naver.com>
2025-01-15 12:52:16 +0900

    01.15 main.js 생성 , classpage.js 생성및수정 main.css 재수정 ,main.html 재수정했습니다 <메인 컨테이너 마우스커서 변경안되게 , 푸터 이동경로 , 베너 이동경로및 텍스트변경 > hsl

--- src/main/resources/static/js/classpage.js
+++ src/main/resources/static/js/classpage.js
@@ -1,4 +1,4 @@
 function navigateToLearningPage() {
     // 이동할 페이지 URL을 설정
-    window.location.href = "learning_page.html"; // 이동할 페이지 경로를 작성
+    window.location.href = "Questionpage.html"; // 이동할 페이지 경로를 작성
 }
---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,4 @@
+function goToLearningPage() {
+    // 이동할 URL을 지정합니다.
+    window.location.href = "classpage.html"; // 여기에 이동할 학습 페이지 URL을 넣으세요.
+}
