f0da0862c809ec41e22fdc2d4bc1be1af050ddf4
sora0301 <qoavkdldj128@naver.com>
2025-01-18 15:11:30 +0900

    01.17 classpage.html,js - js 파일연결오류수정 재화 아이콘클릭시 기능 상점이동 기능추가 왼쪽 사이드바 메뉴 버튼 연결수정 별버튼 클릭시 스테이지에 맞는 문제 이동 수정 MainController - classpage 에서로그아웃버튼 클릭시 메인으로 이동 추가  Purchase_points.html - 외부 스타일이나 이미지 주소 수정   Purchase_points.js - 코드 위치정리   Quest_Progress.html - 이미지 주소수정 ,일일 퀘스트 완료버튼 , 돌아가기클릭시 classpage로 이동으로 수정   Questionpage.css - 스테이지별 진행바 변경   Questionpage.html - script 를 js 로 이동
    hsl

--- src/main/java/com/abc/doge/controller/MainController.java
+++ src/main/java/com/abc/doge/controller/MainController.java
@@ -9,7 +9,7 @@ public class MainController {
     // 메인화면
     @GetMapping("/doge")
     public String showMain() {
-        return "main";
+        return "main";  // "src/main/resources/templates/main.html" 반환
     }
 
     //학습페이지로 이동
@@ -17,4 +17,9 @@ public class MainController {
     public String classpage() {
         return "classpage"; // templates/classpage.html 파일 반환
     }
-}
+    // 메인 페이지 요청 처리
+    @GetMapping("/main")
+    public String showMainPage() {
+        return "main"; // "src/main/resources/templates/main.html" 반환
+    }
+    }
